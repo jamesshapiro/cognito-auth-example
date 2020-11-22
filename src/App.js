a25fa194625ac2855814086de37a12479a70323d
James Shapiro <jamesshapiro@gmail.com>
2020-11-21 19:59:33 -0800

    add starter code for cognito example project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import SignUp from "./SignUp";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SignUp />
+      </div>
+    );
+  }
+}
+
+export default App;
